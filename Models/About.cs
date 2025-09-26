138f63d75d0b92a2e6a486c9a9879cd9d618ca4e
Роман Зорин <sgena170315@gmail.com>
2025-09-26 12:11:25 +0300

    Были добавлены классы-модели About.cs и Note.cs

---
+++ Models/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zorin_Roman_ISP_232_Lab20_MAUiFirstProject.Models
+{
+    internal class About
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "Приложение на .NET MAUI";
+    }
+}
---
+++ Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zorin_Roman_ISP_232_Lab20_MAUiFirstProject.Models
+{
+    internal class Note
+    {
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
