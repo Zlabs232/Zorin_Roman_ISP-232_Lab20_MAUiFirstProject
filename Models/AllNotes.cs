fe0be6567237056da7ed6407eb825ca039fffb89
Роман Зорин <sgena170315@gmail.com>
2025-09-26 12:50:17 +0300

    Добавление представления и модель, в которой перечислены все заметки

---
+++ Models/AllNotes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+namespace Zorin_Roman_ISP_232_Lab20_MAUiFirstProject.Models
+{
+    internal class AllNotes
+    {
+        public ObservableCollection<Note> Notes { get; set; } = new ObservableCollection<Note>();
+
+        public AllNotes()
+        {
+            LoadNotes();
+        }
+
+        public void LoadNotes()
+        {
+            Notes.Clear();
+            string appDataPath = FileSystem.AppDataDirectory;
+            IEnumerable<Note> notes = Directory
+                .EnumerateFiles(appDataPath, "*.notes.txt")
+                .Select(filename => new Note()
+                {
+                    Filename = filename,
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetLastWriteTime(filename)
+                })
+                .OrderBy(note => note.Date);
+
+            foreach (Note note in notes)
+            {
+                Notes.Add
+            }
+        }
+    }
+}
