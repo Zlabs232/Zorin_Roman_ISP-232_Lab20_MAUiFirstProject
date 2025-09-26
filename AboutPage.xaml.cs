236e590b98acc920f05ba07111d57a88aeff979b
Роман Зорин <sgena170315@gmail.com>
2025-09-26 11:34:34 +0300

    Был добавлен LearnMore_Clicked, код обработчика событий

--- AboutPage.xaml.cs
+++ AboutPage.xaml.cs
@@ -2,6 +2,10 @@ namespace Zorin_Roman_ISP_232_Lab20_MAUiFirstProject;
 
 public partial class AboutPage : ContentPage
 {
+	private async void LearnMore_Clicked(object sender, EventArgs e)
+	{
+		await Launcher.Default.OpenAsync("https://aka.ms/maui");
+	}
 	public AboutPage()
 	{
 		InitializeComponent();
