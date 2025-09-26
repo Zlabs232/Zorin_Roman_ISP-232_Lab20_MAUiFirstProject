a6cbcbfb30eb7070845dd56bfdfd6aae9ed41cfe
Роман Зорин <sgena170315@gmail.com>
2025-09-26 11:56:53 +0300

    Работа в файле NotePage.xaml.cs(добавлние обработчика кнопок, работа с файлом(notes.txt))

--- NotePage.xaml.cs
+++ NotePage.xaml.cs
@@ -2,8 +2,26 @@ namespace Zorin_Roman_ISP_232_Lab20_MAUiFirstProject;
 
 public partial class NotePage : ContentPage
 {
+	string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
 	public NotePage()
 	{
 		InitializeComponent();
+
+		if(File.Exists(_fileName))
+			TextEditor.Text = File.ReadAllText(_fileName);
+	}
+
+	public void SaveButton_Clicked(object sender, EventArgs e)
+	{
+		File.WriteAllText(_fileName, TextEditor.Text);
+	}
+
+	public void DeleteButton_Clicked(object sender, EventArgs e)
+	{
+		if(File.Exists(_fileName))
+			File.Delete(_fileName);
+
+		TextEditor.Text = string.Empty;
 	}
 }
